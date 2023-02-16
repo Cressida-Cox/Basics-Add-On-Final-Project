afdc917a5a4f878545f1e331b7a9790c61b54a94
Cressida Cox <cox.cressie@yahoo.co.uk>
2023-02-16 21:17:33 +0000

    Added HTML / CSS / JS

---
+++ src/apps.js
@@ -0,0 +1,8 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+
+  body.classList.toggle("dark");
+}
+
+let themeButton = document.querySelector(".themeButton");
+themeButton.addEventListener("click", changeTheme);
